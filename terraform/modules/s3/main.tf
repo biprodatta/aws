7dadaedeba3c562974279cc844532cd7717dcc8d
biprodatta <biprodatta@gmail.com>
2023-09-20 11:12:19 -0400

    s3 module upload

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,38 +1,68 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "datta-${var.environment}-bucket"
+  bucket = var.environment == "prod" ? "google-central-search-engine" : "google-${var.environment}-central-search-engine"
 }
 
-resource "aws_s3_bucket_policy" "allow_public_access_for_s3_bucket" {
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access_for_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
   policy = data.aws_iam_policy_document.s3_public_readonly.json
+  depends_on = [ aws_s3_bucket.s3_bucket ]
+}
+
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket     = aws_s3_bucket.s3_bucket.id
+  acl        = "private"
+  depends_on = [
+    aws_s3_bucket.s3_bucket,
+    aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership
+  ]
 }
 
 data "aws_iam_policy_document" "s3_public_readonly" {
   statement {
-    sid    = "s3publicreadonly"
+    sid    = "Permissionsonobjectsandbuckets"
     effect = "Allow"
 
     resources = [
-      "aws_s3_bucket.s3_bucket.arn",
-      "${aws_s3_bucket.s3_bucket.arn}/*"
+      var.environment == "prod" ? "arn:aws:s3:::google-central-search-engine" : "arn:aws:s3:::google-${var.environment}-central-search-engine",
+      var.environment == "prod" ? "arn:aws:s3:::google-central-search-engine/*" : "arn:aws:s3:::google-${var.environment}-central-search-engine/*"
     ]
 
     actions = [
-      "s3:GetObject",
-      "s3:ListBucket"
+      "s3:List*",
+      "s3:PutBucketVersioning",
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject"
     ]
 
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::227067764165:role/ec2_full_access"]
     }
   }
-}
+  statement {
+    sid    = "Permissiontooverridebucketowner"
+    effect = "Allow"
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket     = aws_s3_bucket.s3_bucket.id
-  acl        = "public-read"
-  depends_on = [
-    aws_s3_bucket.s3_bucket
-  ]
+    resources = [
+      var.environment == "prod" ? "arn:aws:s3:::google-central-search-engine" : "arn:aws:s3:::google-${var.environment}-central-search-engine",
+      var.environment == "prod" ? "arn:aws:s3:::google-central-search-engine/*" : "arn:aws:s3:::google-${var.environment}-central-search-engine/*"
+    ]
+
+    actions = [
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::227067764165:root"]
+    }
+  }
 }
